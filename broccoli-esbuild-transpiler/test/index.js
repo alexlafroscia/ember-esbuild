9bce3b88e49eec1e23cc40e751c18afec671ae84
Alex LaFroscia <alex@lafroscia.com>
2020-11-17 21:06:12 -0500

    test: add test for JSX transformation

--- broccoli-esbuild-transpiler/test/index.js
+++ broccoli-esbuild-transpiler/test/index.js
@@ -3,7 +3,7 @@
 const ESBuild = require("../index.js");
 const { createBuilder, createTempDir } = require("broccoli-test-helper");
 
-QUnit.test("it transforms an input file", async (assert) => {
+QUnit.test("it can transform TypeScript to JavaScript", async (assert) => {
   assert.expect(1);
 
   const input = await createTempDir();
@@ -27,6 +27,40 @@ QUnit.test("it transforms an input file", async (assert) => {
       // Did the transform work correctly?
       assert.deepEqual(output.read(), {
         "index.js": 'let x = "foo";\n',
+      });
+    } finally {
+      await output.dispose();
+    }
+  } finally {
+    await input.dispose();
+  }
+});
+
+QUnit.test("it can remove JSX from a file", async (assert) => {
+  assert.expect(1);
+
+  const input = await createTempDir();
+
+  try {
+    const subject = new ESBuild(
+      input.path(),
+      { loader: "jsx" },
+      { extensions: ["js"], targetExtension: "js" }
+    );
+    const output = createBuilder(subject);
+
+    try {
+      // Create a faux TypeScript file
+      input.write({
+        "index.js": '<div class="foo"></div>\n',
+      });
+
+      await output.build();
+
+      // Did the transform work correctly?
+      assert.deepEqual(output.read(), {
+        "index.js":
+          '/* @__PURE__ */ React.createElement("div", {\n  class: "foo"\n});\n',
       });
     } finally {
       await output.dispose();
